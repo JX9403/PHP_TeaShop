4954cbb1086adedf5b9e6496672204736984bb9d
Doan Diem <diemdt9403@gmail.com>
2024-11-23 02:17:17 +0700

    completed

---
+++ manager_user/config.php
@@ -0,0 +1,26 @@
+<!-- cac hang so -->
+<?php
+
+// if (!defined('_CODE')) {
+//   die('Access denied...');
+// }
+
+// Thông tin kết nối
+const _HOST = 'localhost';
+const _DB = 'manage-user';
+const _USER = 'root';
+const _PASS = '';
+
+
+const _MODULE = 'home';
+const _ACTION = 'dashboard';
+const _CODE = true;
+
+define('_WEB_HOST', 'http://' . $_SERVER['HTTP_HOST'] . "/ProjectPHP01/manager_user");
+
+define('_WEB_HOST_TEMPLATES', 'http://' . $_SERVER['HTTP_HOST'] . "/ProjectPHP01/manager_user/templates");
+
+define('_WEB_PATH', __DIR__);
+define('_WEB_PATH_TEMPLATES', __DIR__ . '/templates');
+
+
---
+++ manager_user/includes/database.php
@@ -0,0 +1,107 @@
+<?php
+
+if (!defined('_CODE')) {
+  die('Access denied...');
+}
+
+function query($sql, $data = [], $check = false)
+{
+  global $conn;
+  $ketqua = false;
+  try {
+    $statement = $conn->prepare($sql);
+    if (!empty($data)) {
+      $ketqua = $statement->execute($data);
+    } else {
+      $ketqua = $statement->execute();
+
+    }
+  } catch (Exception $e) {
+    echo $e->getMessage() . '<br>';
+    echo 'File: ' . $e->getCode() . '<br>';
+    echo 'Line: ' . $e->getLine();
+    die();
+  }
+
+  if ($check === true) {
+    return $statement;
+  }
+  return $ketqua;
+}
+// Hàm insert vào db
+function insert($table, $data)
+{
+  $key = array_keys($data);
+  $truong = implode(',', $key);
+  $valuetb = ':' . implode(',:', $key);
+
+  $sql = 'INSERT INTO ' . $table . ' (' . $truong . ') VALUES (' . $valuetb . ')';
+
+  $kq = query($sql, $data);
+
+  return $kq;
+}
+
+function update($table, $data, $condition = '')
+{
+  $update = '';
+  foreach ($data as $key => $value) {
+    $update .= $key . '= :' . $key . ',';
+  }
+  $update = trim($update, ',');
+
+  if (!empty($condition)) {
+    $sql = 'UPDATE ' . $table . ' SET ' . $update . ' WHERE ' . $condition;
+  } else {
+    $sql = 'UPDATE ' . $table . ' SET ' . $update;
+  }
+
+
+  $kq = query($sql, $data);
+
+  return $kq;
+}
+
+
+function delete($table, $condition = '')
+{
+    if (!empty($condition)) {
+        $sql = 'DELETE FROM ' . $table . ' WHERE ' . $condition; // Thêm điều kiện WHERE
+    } else {
+        $sql = 'DELETE FROM ' . $table; // Không có điều kiện xóa toàn bộ bảng
+    }
+    $kq = query($sql);
+    return $kq;
+}
+
+
+// lay nhieu dong du lieu
+function getRaw($sql)
+{
+  $kq = query($sql, '', true);
+  if (is_object($kq)) {
+    $dataFetch = $kq->fetchAll(PDO::FETCH_ASSOC);
+  }
+  return $dataFetch;
+}
+
+// Lay 1 dong du lieu
+
+function oneRaw($sql)
+{
+  $kq = query($sql, '', true);
+  if (is_object($kq)) {
+    $dataFetch = $kq->fetch(PDO::FETCH_ASSOC);
+  }
+  return $dataFetch;
+}
+
+// Dem so dong du lieu
+function getRows($sql)
+{
+  $kq = query($sql, '', true);
+  if (!empty($kq)) {
+    return $kq->rowCount();
+  }
+
+}
